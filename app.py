bf6bb1004539e1f926dd5f7430f4660b596116bc
mrGunar <plotkin.andrei@gmail.com>
2022-05-24 12:04:14 +0300

    added module argparse to app

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from game import Game
 from conf import Config
 import os
+import argparse
+
 
 if __name__ == "__main__":
-    game = Game(count_monsters=Config.COUNT_MONSTER.value)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--monsters", help="number of monsters in game",
+                    type=int, default=Config.COUNT_MONSTER.value)
+    args = parser.parse_args()
+
+
+    game = Game(count_monsters=args.monsters)
     os.system("cls")
     game.run()
